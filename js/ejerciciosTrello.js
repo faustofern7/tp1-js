94a0fa966ee97c2412fe2f95c3bb98f0bdb1e1f0
Fausto Fernandez <fausto44190084@gmail.com>
2023-12-26 20:19:54 -0300

    realice el ejercicio 2 de Trello

--- js/ejerciciosTrello.js
+++ js/ejerciciosTrello.js
@@ -6,3 +6,9 @@ document.write(" <br> El resultado de la resta es: "  + (numero1 - numero2));
 document.write(" <br> El resultado de la multiplicación es: "  + (numero1 * numero2));
 document.write(" <br> El resultado de la division es: "  + (numero1 / numero2));
 
+let nombre = "Fausto Fidel";
+let apellido = " Fernández";
+
+let nombreCompleto = nombre +  apellido ;
+
+document.write("<br> el nombre completo es: " + nombreCompleto);
