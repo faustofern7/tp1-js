c9ffcf50353a45c3d1a2b4ddb6a49a1c64e991e5
Fausto Fernandez <fausto44190084@gmail.com>
2023-12-26 20:14:00 -0300

    realice el ejercicio 1 de trello

---
+++ js/ejerciciosTrello.js
@@ -0,0 +1,8 @@
+let numero1 = 20;
+let numero2 = 10;
+
+document.write("El resultado de la suma es: "  + (numero1 + numero2) );
+document.write(" <br> El resultado de la resta es: "  + (numero1 - numero2));
+document.write(" <br> El resultado de la multiplicación es: "  + (numero1 * numero2));
+document.write(" <br> El resultado de la division es: "  + (numero1 / numero2));
+
