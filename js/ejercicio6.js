91fa79f8f081a7c87f686b706983efdf434c8d21
Fausto Fernandez <fausto44190084@gmail.com>
2023-12-26 21:46:07 -0300

    realice el ejercicio6 del tp1 de js

---
+++ js/ejercicio6.js
@@ -0,0 +1,10 @@
+let numeroo1 = parseInt(prompt('Ingrese el primer número:'));
+let numeroo2 = parseInt(prompt('Ingrese el segundo número:'));
+
+    if (numero1 > numero2) {
+      document.write('El número mayor es: ' + numero1);
+    } else if (numero2 > numero1) {
+      document.write('El número mayor es: ' + numero2);
+    } else {
+      document.write('Los números son iguales');
+    }
