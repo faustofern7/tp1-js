c103e8f3227e99200b8cbac800716732caef0c8b
Fausto Fernandez <fausto44190084@gmail.com>
2023-12-26 21:27:38 -0300

    realice el ejercicio 5 del tp1 de js

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+let num1 = parseInt(prompt('Ingrese un valor numerico: '));
+let num2 = parseInt(prompt('Ingrese nuevamente un valor numerico: '));
+document.write('<br>El resultado de sumar ' + num1 + ' y ' + num2 + ' es: ' + (num1 + num2) + '<br>');
