a7d6e34c56967d689d0395cf2e8b9d3ce8d82341
Fausto Fernandez <fausto44190084@gmail.com>
2023-12-26 22:00:46 -0300

    realice el ejercicio 7 del tp1 de js

---
+++ js/ejercicio7.js
@@ -0,0 +1,13 @@
+let numero1_ = parseFloat(prompt("Ingrese el primer número:"));
+let numero2_ = parseFloat(prompt("Ingrese el segundo número:"));
+let numero3_ = parseFloat(prompt("Ingrese el tercer número:"));
+
+    if (numero1_ > numero2_ && numero1_ > numero3_) {
+      document.write("<br>El número mayor es: " + numero1);
+    } else if (numero2_ > numero1_ && numero2_ > numero3_) {
+      document.write("<br>El número mayor es: " + numero2_);
+    } else if (numero3_ > numero1_ && numero3_ > numero2_) {
+      document.write("<br>El número mayor es: " + numero3_);
+    } else {
+      document.write("<br>Los números son iguales.");
+    }
